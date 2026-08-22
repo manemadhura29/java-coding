da6ab219186bdd6789c46c6c047286fe38e4dd8f
Madhura Mane <manemadhura29@gmail.com>
2026-08-22 20:52:40 +0530

    Add files via upload

---
+++ p_test_1.java
@@ -0,0 +1,21 @@
+//Practice Test No -01 22/08/26
+
+import java.util.*;
+
+class p_test_1
+{
+	public static void main(String args[])
+	{
+		int arr[]={10,20,30,40};
+		int k=1;
+
+		for(int i=k; i<arr.length; i++)
+		{
+			System.out.print(arr[i] +" ");
+		}
+		for(int i=0; i<k; i++)
+		{
+			System.out.print(arr[i] +" ");
+		}
+	}
+}
